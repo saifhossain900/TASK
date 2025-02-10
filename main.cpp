df993fdff7416fa3b70fbb23b28b8d39428279b4
saifhossain900 <107342341+saifhossain900@users.noreply.github.com>
2025-02-10 09:25:24 +0600

    Add files via upload
    TASK 01

--- main.cpp
+++ main.cpp
@@ -4,31 +4,20 @@ using namespace std;
 
 int main()
 {
-     int row, column;                             // Variables to store user specified row and column size
-    cin >> row >> column;                        // Taking inputs and storing row and column size
-
-    int colArray[row];                           // Initialize colArray to store different column size in row
-    int **ptr = new int *[row];                  // Allocating a pointer array to be used to store "row" array addresses
-
-    for (int i = 0; i < row; i++) {
-        cin >> column;                           // Taking and storing the inputs of specified row size
-        ptr[i] = new int[column];                // Dynamically allocating array size for each row
-        colArray[i] = column;                    // Storing different column size to keep track
+    int array_size;
+    cin >> array_size;
+    int* ptr = new int[array_size];
+    for(int i=0 ; i<array_size ; i++)
+    {
+        cin >> ptr[i];
     }
-
-    for (int i = 0; i < row; i++) {              // Taking input and saving its addresses on the arrays
-        for (int j = 0; j < colArray[i]; j++) {
-            cin >> ptr[i][j];
-        }
+    for (int j=0 ; j<array_size ; j++)
+    {
+        cout << ptr[j] << " ";
     }
-
-    for (int i = 0; i < row; i++) {              // Printing each values of the arrays
-        for (int j = 0; j < colArray[i]; j++) {
-            cout << ptr[i][j] << " ";
-        }
-        cout << endl;
-    }
+    cout << endl;
 
     delete[] ptr;
+
     return 0;
 }
