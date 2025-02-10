bcdb304d6b3560aee06e5f622d9273d1d6ddb3a5
saifhossain900 <107342341+saifhossain900@users.noreply.github.com>
2025-02-10 09:24:15 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 int main()
 {
-    int row, column;
-    cin >> row >> column;
+     int row, column;                             // Variables to store user specified row and column size
+    cin >> row >> column;                        // Taking inputs and storing row and column size
 
-    int **ptr = new int *[row];
+    int colArray[row];                           // Initialize colArray to store different column size in row
+    int **ptr = new int *[row];                  // Allocating a pointer array to be used to store "row" array addresses
 
     for (int i = 0; i < row; i++) {
-        ptr[i] = new int[column];
+        cin >> column;                           // Taking and storing the inputs of specified row size
+        ptr[i] = new int[column];                // Dynamically allocating array size for each row
+        colArray[i] = column;                    // Storing different column size to keep track
     }
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
+    for (int i = 0; i < row; i++) {              // Taking input and saving its addresses on the arrays
+        for (int j = 0; j < colArray[i]; j++) {
             cin >> ptr[i][j];
         }
     }
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
+    for (int i = 0; i < row; i++) {              // Printing each values of the arrays
+        for (int j = 0; j < colArray[i]; j++) {
             cout << ptr[i][j] << " ";
         }
         cout << endl;
